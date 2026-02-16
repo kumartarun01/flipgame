a9470f8ab7a8eae67ce57e6094acc723a4b5d096
Tarun Kumar <jaatrony84@gmail.com>
2026-02-16 14:50:08 +0530

    Initial Commit

---
+++ flipgame/flipgameApp.swift
@@ -0,0 +1,17 @@
+//
+//  flipgameApp.swift
+//  flipgame
+//
+//  Created by iMac1 on 16/02/26.
+//
+
+import SwiftUI
+
+@main
+struct flipgameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
